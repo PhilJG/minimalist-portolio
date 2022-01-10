90da937e6681ca71425d9bc16da7b8503f4d8608
Phil <phl.j.gray@gmail.com>
2022-01-09 20:07:11 -0800

    third commit

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 "use strict";
+
+const navList = document.getElementsByClassName("nav__list");
+const toggle = document.getElementById("toggle");
+const toggleOff = document.getElementById("toggle-off");
+
+function navVisible() {
+  if ((navList.classList.visibility = "hidden")) {
+    navList.classList.add(".block") = "visible";
+    toggle.style.visibility = "hidden";
+    toggleOff.style.visibility = "visible";
+  }
+  console.log("visible");
+}
+
+function navInvisible() {
+  if ((navList.style.visibility = "visible")) {
+    navList.style.visibility = "hidden";
+    toggle.style.visibility = "visible";
+    toggleOff.style.visibility = "hidden";
+  }
+  console.log("invisible");
+}
